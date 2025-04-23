6a5c484e0689da6934f4e278621e97df3b5c23b4
eahmed7 <ehanahmed@lawn-10-91-18-231.lawn.gatech.edu>
2025-04-23 18:23:21 -0400

    logout button  and report page

--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,20 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('about/', views.about, name='about'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('marketplace/', views.marketplace, name='marketplace'),
+    path('collection/', views.collection, name='collection'),
+    path('trade/', views.trade, name='trade'),
+    path('logout/', views.logout_view, name='logout'),
+    path('report/', views.report_issue, name='report'),
+
+
+
+
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import login as auth_login, logout
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.mail import send_mail
+
+
 
 def index(request):
     return render(request, 'home/index.html');
@@ -21,10 +26,41 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('index')
+            return redirect(resolve_url(settings.LOGIN_REDIRECT_URL))
     else:
        form = AuthenticationForm()
     return render(request, 'home/login.html', {'form': form})
 
 def about(request):
     return render(request, 'home/about.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'home/dashboard.html')
+
+def marketplace(request):
+    return render(request, 'home/marketplace.html')
+
+def collection(request):
+    return render(request, 'home/collection.html')
+
+def trade(request):
+    return render(request, 'home/trade.html')
+
+@login_required
+def report_issue(request):
+    if request.method == 'POST':
+        issue_type = request.POST.get('issue_type')
+        description = request.POST.get('description')
+        subject = f"New {issue_type.capitalize()} Report from {request.user.username}"
+        message = f"Issue Type: {issue_type}\nUser: {request.user.username}\n\nDescription:\n{description}"
+
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [settings.ADMIN_EMAIL])
+
+        return redirect('dashboard')  # or a success page
+
+    return render(request, 'report.html')
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+

f515fbca5c64c9c7ff6fb7275463fadf1704496e
eahmed7 <ehanahmed@lawn-10-91-18-231.lawn.gatech.edu>
2025-04-23 16:38:56 -0400

    register, about, login, some admin stuff

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('about/', views.about, name='about'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 def index(request):
     return render(request, 'home/index.html');
 
 def register(request):
-    return render(request, 'register.html');
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'home/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('index')
+    else:
+       form = AuthenticationForm()
+    return render(request, 'home/login.html', {'form': form})
+
+def about(request):
+    return render(request, 'home/about.html')
